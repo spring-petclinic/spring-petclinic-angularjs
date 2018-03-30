605e9ed354ff9ba7ec026cac7f7e0d48af5c0a35
Antoine Rey <antoine.rey@gmail.com>
2018-03-30 19:32:32 +0200

    Use the ExpiryPolicyBuilder

--- spring-petclinic-server/src/main/java/org/springframework/samples/petclinic/config/CacheConfig.java
+++ spring-petclinic-server/src/main/java/org/springframework/samples/petclinic/config/CacheConfig.java
@@ -2,19 +2,14 @@ package org.springframework.samples.petclinic.config;
 
 import org.ehcache.config.CacheConfiguration;
 import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.ExpiryPolicyBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
 import org.ehcache.config.units.EntryUnit;
-import org.ehcache.expiry.Duration;
-import org.ehcache.expiry.Expirations;
 import org.ehcache.jsr107.Eh107Configuration;
 import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-import javax.cache.CacheManager;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Cache could be disable in unit test.
@@ -25,17 +20,14 @@ public class CacheConfig {
 
     @Bean
     public JCacheManagerCustomizer cacheManagerCustomizer() {
-        return new JCacheManagerCustomizer() {
-            @Override
-            public void customize(CacheManager cacheManager) {
-                CacheConfiguration<Object, Object> config = CacheConfigurationBuilder
-                    .newCacheConfigurationBuilder(Object.class, Object.class,
-                        ResourcePoolsBuilder.newResourcePoolsBuilder()
-                            .heap(100, EntryUnit.ENTRIES))
-                    .withExpiry(Expirations.timeToLiveExpiration(Duration.of(60, TimeUnit.SECONDS)))
-                    .build();
-                cacheManager.createCache("vets", Eh107Configuration.fromEhcacheCacheConfiguration(config));
-            }
+        return cacheManager -> {
+            CacheConfiguration<Object, Object> config = CacheConfigurationBuilder
+                .newCacheConfigurationBuilder(Object.class, Object.class,
+                    ResourcePoolsBuilder.newResourcePoolsBuilder()
+                        .heap(100, EntryUnit.ENTRIES))
+                .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(java.time.Duration.ofSeconds(60)))
+                .build();
+            cacheManager.createCache("vets", Eh107Configuration.fromEhcacheCacheConfiguration(config));
         };
     }
 
