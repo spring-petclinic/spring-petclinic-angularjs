583dfee66be9449bf98ed13cf0305b6bac3fdab9
Antoine Rey <antoine.rey@gmail.com>
2018-03-30 19:26:21 +0200

    Change the @NotEmpty error message

--- spring-petclinic-server/src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ spring-petclinic-server/src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -13,14 +13,14 @@ import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 /**
- * 
+ *
  * @author Michael Isvy
- * Simple test to make sure that Bean Validation is working 
+ * Simple test to make sure that Bean Validation is working
  * (useful when upgrading to a new version of Hibernate Validator/ Bean Validation)
  *
  */
 public class ValidatorTests {
-	
+
 	private Validator createValidator() {
 	      LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
 	      localValidatorFactoryBean.afterPropertiesSet();
@@ -41,7 +41,7 @@ public class ValidatorTests {
         assertThat(constraintViolations.size()).isEqualTo(1);
         ConstraintViolation<Person> violation =  constraintViolations.iterator().next();
         assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
-        assertThat(violation.getMessage()).isEqualTo("may not be empty");
+        assertThat(violation.getMessage()).isEqualTo("must not be empty");
     }
-	
+
 }
