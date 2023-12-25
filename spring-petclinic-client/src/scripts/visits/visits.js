29365086f0f20ab077c8d991e3ed3c025de9105d
Imad Eddine <93336146+iebouihi@users.noreply.github.com>
2023-12-25 12:18:58 +0100

    Update visits.js

--- spring-petclinic-client/src/scripts/visits/visits.js
+++ spring-petclinic-client/src/scripts/visits/visits.js
@@ -9,3 +9,44 @@ angular.module('visits', ['ui.router'])
                 template: '<visits></visits>'
             })
     }]);
+function aaToooLong_Non_MeaningfulMethodeBlaBlaBlaBlaaaToooLong_Non_MeaningfulMethodeBlaBlaBlaBla()
+{
+or (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      const b = bricks[c][r];
+      if (b.status === 1) {
+        if (
+          x > b.x &&
+          x < b.x + brickWidth &&
+          y > b.y &&
+          y < b.y + brickHeight
+        ) {
+          dy = -dy;
+          b.status = 0;
+          score++;
+        }
+      }
+    }
+  }
+}
+
+function aaToooLong_Non_MeaningfulMethodeBlaBlaBlaBlaaaToooLong_Non_MeaningfulMethodeBlaBlaBlaBla2222()
+{
+or (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      const b = bricks[c][r];
+      if (b.status === 1) {
+        if (
+          x > b.x &&
+          x < b.x + brickWidth &&
+          y > b.y &&
+          y < b.y + brickHeight
+        ) {
+          dy = -dy;
+          b.status = 0;
+          score++;
+        }
+      }
+    }
+  }
+}
