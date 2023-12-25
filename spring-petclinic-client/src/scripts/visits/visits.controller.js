1057e2f2c8652a43b15947469fa230c78751db0a
Imad Eddine <93336146+iebouihi@users.noreply.github.com>
2023-12-25 12:45:26 +0100

    Update visits.controller.js

--- spring-petclinic-client/src/scripts/visits/visits.controller.js
+++ spring-petclinic-client/src/scripts/visits/visits.controller.js
@@ -23,3 +23,23 @@ angular.module('visits')
             });
         };
     }]);
+function aaToooLong_Non_MeaningfulMethodeBlaBlaBlaBlaaaToooLong_Non_MeaningfulMethodeBlaBlaBlaBla()
+{
+for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      const b = bricks[c][r];
+      if (b.status === 1) {
+        if (
+          x > b.x &&
+          x < b.x + brickWidth &&
+          y > b.y &&
+          y < b.y + brickHeight
+        ) {
+          dy = -dy;
+          b.status = 0;
+          score++;
+        }
+      }
+    }
+  }
+}
